c19e196ae32ffff128ab49e94cb163cfb9f7f709
Muna Abdullahi <devmuna25@gmail.com>
2026-03-08 14:19:39 +0300

    submitted Ex4 and Ex5

---
+++ Ex5/main.js
@@ -0,0 +1,7 @@
+// EXERCISE 5
+const Add = (x,y) => {
+    return x + y;
+    
+}
+console.log("The sum is: " + Add(10, 20));
+console.log("The sum is: " + Add(100, 20));
---
+++ Exer4/Script.js
@@ -0,0 +1,17 @@
+// Exercise 4
+//here is the function declaration
+function Add(a,b) {
+    return a + b;
+}
+let Sum = Add(10, 20);
+console.log(Sum);
+ let Sum2 = Add(100, 20);
+console.log(Sum2);
+
+// function expression
+
+const add = function(a,b) {
+    return a + b;
+}
+console.log(add(10, 20));
+console.log(add(100, 20));
