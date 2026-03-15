6ddc0d7feff04109adea50ac686241f6fada61af
Muna Abdullahi <devmuna25@gmail.com>
2026-03-15 23:49:53 +0300

    submitted ex8

---
+++ EX8/main.js
@@ -0,0 +1,10 @@
+let car = {
+    make: "Toyota",
+    model : "BMW",
+    year : 2020,
+    color : "red",
+}
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+console.log(car.color);
