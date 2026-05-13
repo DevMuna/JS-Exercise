d821a1dc6164421990af90eebe911e1c03e44d35
Muna Abdullahi <devmuna25@gmail.com>
2026-05-13 14:44:54 +0300

    submitting ex14

---
+++ Ex14/main.js
@@ -0,0 +1,6 @@
+let num;
+do {
+    num = prompt("Enter a number greater than 10:");
+} while(num > 10);
+
+console.log("Thank you for entering a valid number: " + num);
--- README.md
+++ README.md
@@ -39,3 +39,5 @@ In this repository, I will share all my JavaScript exercises and practice tasks 
 
 
 
+
+
