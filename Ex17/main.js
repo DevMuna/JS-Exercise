a46f0cff627ee9773db911317481423213140205
Muna Abdullahi <devmuna25@gmail.com>
2026-05-19 20:27:05 +0300

    submitted ex17

---
+++ Ex17/main.js
@@ -0,0 +1,13 @@
+let temperature = 20
+if(temperature < 0) {
+    console.log("very cold")
+} else if (temperature > 0  && temperature < 15) {
+    console.log("cold")
+
+} else if(temperature >= 15 && temperature <25) {
+    console.log("warm")
+} else{
+    console.log("hot")
+}
+
+
