6d7ec3858635cac5ca3e2faca09a05e65dbac224
Muna Abdullahi <devmuna25@gmail.com>
2026-05-27 23:41:24 +0300

    Submitting ex

---
+++ Ex25/main.js
@@ -0,0 +1,23 @@
+// here is the exer of spread operator
+const num = [1, 2, 3, 4, 5]
+const Newnum = [...num, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+console.log(Newnum)
+
+
+
+
+// rest operator
+function mult(...numbers) {
+    return numbers.reduce((total, num) => total * num, 1);
+}
+console.log(mult(2, 3, 5, )); 
+console.log(mult(10, 4));
+
+
+
+
+
+
+
+
+
